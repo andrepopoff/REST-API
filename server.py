43702fa4f7783f478b624be0ff307f2528d3142e
andrepopoff <andrepopoff@gmail.com>
2018-11-29 15:21:00 +0400

    Added __slots__ for User class

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 class User(object):
+    __slots__ = ('id_', 'name', 'email', 'age', 'sex')
+    
     def __init__(self, id_, name, email, age, sex):
         self.id_ = id_
         self.name = name
