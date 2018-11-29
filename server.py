319cdef89c68c3b63062fb5c30eb627de63dc38e
andrepopoff <andrepopoff@gmail.com>
2018-11-29 15:14:33 +0400

    Revert "Created server.py file"
    This reverts commit 553364d20ad168f01fb2d4a4024cef9db00e5c11.

--- server.py
+++
@@ -1,7 +0,0 @@
-class User(object):
-    def __init__(self, id_, name, email, age, sex):
-        self.id_ = id_
-        self.name = name
-        self.email = email
-        self.age = age
-        self.sex = sex
