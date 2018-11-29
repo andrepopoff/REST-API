2301ddbe2ce159bbe92694e5d29a1486e5b97d19
andrepopoff <andrepopoff@gmail.com>
2018-11-29 15:23:19 +0400

    Created class UserRef

--- server.py
+++ server.py
@@ -1,9 +1,19 @@
 class User(object):
     __slots__ = ('id_', 'name', 'email', 'age', 'sex')
-    
+
     def __init__(self, id_, name, email, age, sex):
         self.id_ = id_
         self.name = name
         self.email = email
         self.age = age
         self.sex = sex
+
+
+class UserRef(object):
+    __slots__ = ('id_', 'name', 'url')
+
+    def __init__(self, id_, name, url):
+        self.id_ = id_
+        self.name = name
+        self.url = url
+        
