553364d20ad168f01fb2d4a4024cef9db00e5c11
andrepopoff <andrepopoff@gmail.com>
2018-11-29 15:11:19 +0400

    Created server.py file

---
+++ server.py
@@ -0,0 +1,7 @@
+class User(object):
+    def __init__(self, id_, name, email, age, sex):
+        self.id_ = id_
+        self.name = name
+        self.email = email
+        self.age = age
+        self.sex = sex
